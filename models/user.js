3ccaa05ef9785f82f0925d7520265efc53969746
zainabahmed <zainabahmedlll19@gmail.com>
2026-08-06 14:22:09 +0300

    adding email to model

--- models/user.js
+++ models/user.js
@@ -9,6 +9,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    email: {
+        type: String,
+        required: true,
+    }
 }, {timestamps: true})
 
 userSchema.set('toJSON', {
