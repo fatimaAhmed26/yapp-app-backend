8e02e118e76655018172c205096ccc8e6ea8e8fe
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-08-06 15:21:54 +0300

    installing cloudinary

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+
+
 const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
 const morgan = require('morgan')
+
+const upload = require('./config/multer')
+
 
 const PORT = process.env.PORT ? process.env.PORT : "3000"
 
